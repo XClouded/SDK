11b004040419cb2eaca29e7474f6aa8109129874
yongbo000 <git5421>
2013-01-30 09:45:50 +0800

    下载管理类

---
+++ SDK/src/me/yongbo/sdk/ApkDownloadManager.java
@@ -0,0 +1,238 @@
+package me.yongbo.sdk;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.app.AlertDialog.Builder;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.DialogInterface.OnCancelListener;
+import android.content.DialogInterface.OnClickListener;
+import android.net.Uri;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ApkDownloadManager {
+
+	private static final int DOWN_NOSDCARD = 0;
+    private static final int DOWN_UPDATE = 1;
+    private static final int DOWN_OVER = 2;
+	
+	private static ApkDownloadManager downloadManager;
+	
+	private Context mContext;
+	//通知对话框
+	private Dialog noticeDialog;
+	//下载对话框
+	private Dialog downloadDialog;
+    //进度条
+    private ProgressBar mProgress;
+    //显示下载数值
+    private TextView mProgressText;
+    //查询动画
+    private ProgressDialog mProDialog;
+    //进度值
+    private int progress;
+    //下载线程
+    private Thread downLoadThread;
+    //终止标记
+    private boolean interceptFlag;
+	//提示语
+	private String updateMsg = "";
+	//返回的安装包url
+	private String apkUrl = "";
+	//下载包保存路径
+    private String savePath = "";
+	//apk保存完整路径
+	private String apkFilePath = "";
+	//临时下载文件路径
+	private String tmpFilePath = "";
+	//下载文件大小
+	private String apkFileSize;
+	//已下载文件大小
+	private String tmpFileSize;
+	
+	private List<String> urls = new ArrayList<String>();
+	
+	private Handler mHandler = new Handler(){
+    	public void handleMessage(Message msg) {
+    		switch (msg.what) {
+			case DOWN_UPDATE:
+				mProgress.setProgress(progress);
+				mProgressText.setText(tmpFileSize + "/" + apkFileSize);
+				break;
+			case DOWN_OVER:
+				downloadDialog.dismiss();
+				installApk();
+				break;
+			case DOWN_NOSDCARD:
+				downloadDialog.dismiss();
+				Toast.makeText(mContext, "无法下载安装文件，请检查SD卡是否挂载", 3000).show();
+				break;
+			}
+    	};
+    };
+	public ApkDownloadManager(Context context) {
+		this.mContext = context;
+	}
+
+	public static ApkDownloadManager getInstance(Context context) {
+		if(downloadManager == null){
+			downloadManager = new ApkDownloadManager(context);
+		}
+		downloadManager.interceptFlag = false;
+		return downloadManager;
+	}
+	private Runnable mdownApkRunnable = new Runnable() {	
+		@Override
+		public void run() {
+			try {
+				String apkName = "OSChinaApp_.apk";
+				String tmpApk = "OSChinaApp_.tmp";
+				//判断是否挂载了SD卡
+				if(!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)){
+					//没有挂载SD卡，无法下载文件
+					mHandler.sendEmptyMessage(DOWN_NOSDCARD);
+					return;
+				}
+				String dateNow = new SimpleDateFormat("yyyyMMdd").format(new java.util.Date());
+				savePath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/download/" + dateNow + "/";
+				File file = new File(savePath);
+				//下载文件夹是否存在
+				if(!file.exists()){
+					file.mkdirs();
+				}
+				apkFilePath = savePath + apkName;
+				tmpFilePath = savePath + tmpApk;
+				File ApkFile = new File(apkFilePath);
+				
+				//是否已下载更新文件
+				if(ApkFile.exists()){
+					downloadDialog.dismiss();
+					installApk();
+					return;
+				}
+				
+				//输出临时下载文件
+				File tmpFile = new File(tmpFilePath);
+				FileOutputStream fos = new FileOutputStream(tmpFile);
+				
+				URL url = new URL(apkUrl);
+				HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+				conn.connect();
+				int length = conn.getContentLength();
+				InputStream is = conn.getInputStream();
+				
+				//显示文件大小格式：2个小数点显示
+		    	DecimalFormat df = new DecimalFormat("0.00");
+		    	//进度条下面显示的总文件大小
+		    	apkFileSize = df.format((float) length / 1024 / 1024) + "MB";
+				
+				int count = 0;
+				byte buf[] = new byte[1024];
+				
+				do {   		   		
+		    		int numread = is.read(buf);
+		    		count += numread;
+		    		//进度条下面显示的当前下载文件大小
+		    		tmpFileSize = df.format((float) count / 1024 / 1024) + "MB";
+		    		//当前进度值
+		    	    progress =(int)(((float)count / length) * 100);
+		    	    //更新进度
+		    	    mHandler.sendEmptyMessage(DOWN_UPDATE);
+		    		if(numread <= 0){	
+		    			//下载完成 - 将临时下载文件转成APK文件
+						if(tmpFile.renameTo(ApkFile)){
+							//通知安装
+							mHandler.sendEmptyMessage(DOWN_OVER);
+						}
+		    			break;
+		    		}
+		    		fos.write(buf,0,numread);
+		    	} while(!interceptFlag);//点击取消就停止下载
+				
+				fos.close();
+				is.close();
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+			} catch(IOException e){
+				e.printStackTrace();
+			}
+			
+		}
+	};
+	/**
+	 * 显示下载对话框
+	 */
+	private void showDownloadDialog(){
+		AlertDialog.Builder builder = new Builder(mContext);
+		builder.setTitle("正在下载...");
+		
+		final LayoutInflater inflater = LayoutInflater.from(mContext);
+		View v = inflater.inflate(R.layout.update_progress, null);
+		mProgress = (ProgressBar)v.findViewById(R.id.update_progress);
+		mProgressText = (TextView) v.findViewById(R.id.update_progress_text);
+		
+		builder.setView(v);
+		builder.setNegativeButton("取消", new OnClickListener() {	
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				dialog.dismiss();
+				interceptFlag = true;
+			}
+		});
+		builder.setOnCancelListener(new OnCancelListener() {
+			@Override
+			public void onCancel(DialogInterface dialog) {
+				dialog.dismiss();
+				interceptFlag = true;
+			}
+		});
+		downloadDialog = builder.create();
+		downloadDialog.setCanceledOnTouchOutside(false);
+		downloadDialog.show();
+	}
+	
+	/**
+	* 下载apk
+	* @param url
+	*/	
+	public void doDown(String url) {
+		this.apkUrl = url;
+		showDownloadDialog();
+		downLoadThread = new Thread(mdownApkRunnable);
+		downLoadThread.start();
+	}
+	
+	/**
+    * 安装apk
+    * @param url
+    */
+	private void installApk(){
+		File apkfile = new File(apkFilePath);
+        if (!apkfile.exists()) {
+            return;
+        }    
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setDataAndType(Uri.parse("file://" + apkfile.toString()), "application/vnd.android.package-archive"); 
+        mContext.startActivity(i);
+	}
+}
